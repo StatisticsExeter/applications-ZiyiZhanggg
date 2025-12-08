3fb2f328c6e7067831c5081e0995792a73a67ef7
ZiyiZhanggg <zzpineapple12@gmail.com>
2025-12-08 15:08:30 +0000

    split test train

--- course/supervised_classification/split_test_train.py
+++ course/supervised_classification/split_test_train.py
@@ -17,6 +17,8 @@ def split_data(base_data_path, X_train_path, y_train_path, X_test_path, y_test_p
     df = pd.read_csv(base_data_path).dropna()
     y = df['built_age']
     X = df.drop(columns=['built_age'])
+    X_train, X_test, y_train, y_test = train_test_split(
+      X, y, test_size=0.3, stratify=y, random_state=1999)
     """Form four dataframes, X_train, y_train, X_test, y_test with 30% of the data for testing"""
     X_train.to_csv(X_train_path, index=False)
     y_train.to_csv(y_train_path, index=False)
